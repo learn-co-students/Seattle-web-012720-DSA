c5ba1210af9feb9e600e6431a9ed31f7e8885a65
Hallie Dunn <hallie.dunn1@gmail.com>
2020-03-10 15:22:49 -0700

    clarion's solution

--- 07-array-rotation/solution.js
+++ 07-array-rotation/solution.js
@@ -1,1 +1,27 @@
 // Clarion's solution
+
+function rotate_left(array, rotations) {
+    let mapping = [];
+    let x = array.length;
+    numRotations = rotations%x;
+    for (let i=0; i<x; i++) {
+      let actual = i-numRotations;
+      if (actual < 0) {
+        actual = x+actual;
+      }
+      mapping[actual] = array[i];
+    }
+    return mapping
+}
+
+// or, we can use shift/push:
+
+function rotate_left(array, rotations) {
+    let x = array.length;
+    numRotations = rotations%x;
+    for (let i=0; i<numRotations; i++) {
+      temp = array.shift()
+      array.push(temp)
+      }
+      return array
+}
