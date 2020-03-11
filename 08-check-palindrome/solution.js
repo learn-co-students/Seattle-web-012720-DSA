c3a2d14b7ff6d0e7143bd1e91b49554a0a8f1bb9
Hallie Dunn <hallie.dunn1@gmail.com>
2020-03-11 14:52:27 -0700

    diana's solution

--- 08-check-palindrome/solution.js
+++ 08-check-palindrome/solution.js
@@ -1,1 +1,13 @@
 // Diana
+function checkPalidrome(str) {
+    str.replace(/\s/g, '');
+    let length = str.length;
+    let count = parseInt(length/2);
+    for (let i=0; i < count; i++) {
+      if (str[i] !== str[length - (i+1)]) {
+        return false;
+      }
+      return true;
+    }
+    //true/false
+  }
