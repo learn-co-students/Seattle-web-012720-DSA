108f939f4743bb79473ed2b6051def7b080abfbf
Hallie Dunn <hallie.dunn1@gmail.com>
2020-03-10 17:46:45 -0700

    lief's addition

--- 07-array-rotation/solution.js
+++ 07-array-rotation/solution.js
@@ -25,3 +25,44 @@ function rotate_left(array, rotations) {
       }
       return array
 }
+
+// here is lief's time stamped test runs with shift/push versus slice:
+
+function arrayRotationShiftPush(array, numberOfRotations) {
+  number = numberOfRotations%array.length;
+  for (let i = 0; i < number; i++) {
+      array.push(array.shift());
+  }
+  return array
+}
+
+function arrayRotationSplice(array, numberOfRotations) {
+  index = numberOfRotations%array.length;
+
+  return [...array.splice(index), ...array]
+}
+
+let array = []
+let array2 = []
+for (i = 0; i<20000; i++) {
+  array[i] = i;
+  array2[i] = i;
+}
+
+let time = Date.now();
+const rotations = 1000;
+const runs = 1;
+
+for (i = 0; i < runs; i++) {
+arrayRotationShiftPush(array, rotations);
+}
+console.log(Date.now() - time);
+
+
+time = Date.now();
+
+for (i = 0; i < runs; i++) {
+arrayRotationSplice(array2, rotations);
+}
+
+console.log(Date.now() - time);
