590687d89b2f67cc0335dcdb197af7f176aa2fc3
Hallie Dunn <hallie.dunn1@gmail.com>
2020-03-19 14:41:40 -0700

    jazz

--- 11-adjacent-elements/solution.js
+++ 11-adjacent-elements/solution.js
@@ -1,1 +1,17 @@
 // Jazz
+
+testArray = [2,5,6,3,-7,8,10,-6]
+function adjacentProduct(array) {
+  let x=0
+  let y=0
+  let p=0
+  for(let i=0;i<array.length-1;i++) {
+    x=array[i];
+    y=array[i+1]
+    if (x*y>p) {
+      p= x*y;
+    };
+  };
+  return p;
+};
+adjacentProduct(testArray);
