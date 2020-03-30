afb6a265996f8a9c146f6ac19bf455530130f5e2
Hallie Dunn <hallie.dunn1@gmail.com>
2020-03-30 14:23:46 -0700

    staircase solutions

--- 15-staircase-hashtag/solution.js
+++ 15-staircase-hashtag/solution.js
@@ -1,1 +1,36 @@
 // jonny
+
+function staircase(n) {
+    for(let i=0; i<n+1; i++){
+        let str = ''
+        for(let j=i; j<n; j++){
+            str += ' '
+        }
+        for(let j=0;j<i;j++){
+            str += '#'
+        }
+        console.log(str)
+    }
+}
+
+// chris's internet cheat code solution
+
+function staircase(num) {
+    for (let i=1; i <= num; i++) {
+      let spaces = num - i
+      console.log(" ".repeat(spaces) + "#".repeat(i))
+    }
+  }
+  staircase(4)
+
+
+// clarion's Array.fill surprise
+
+function staircase(n){
+    for (i=1; i<=n; i++){
+      let spaces = Array(n-i).fill(' ');
+      let hashtags = Array(i).fill("#");
+      let line = spaces.concat(hashtags).join('');
+      console.log(line);
+    }
+  }
