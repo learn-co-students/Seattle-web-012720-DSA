46b797a392ce927f48143b8e47fa09ec09db4e98
Hallie Dunn <hallie.dunn1@gmail.com>
2020-03-30 13:32:49 -0700

    chris's solution

--- 14-kangaroo/solution.js
+++ 14-kangaroo/solution.js
@@ -1,1 +1,17 @@
 // chris
+
+
+function kangaroo(x1, v1, x2, v2) {
+    if (x1 == x2 && v1 == v2) {
+      return "YES"
+    }
+    if (x1 == x2 && v1 != v2) {
+      return "NO"
+    }
+    if (x1 != x2 && v1 == v2) {
+      return "NO"
+    }
+  }
+
+
+//   incomplete
