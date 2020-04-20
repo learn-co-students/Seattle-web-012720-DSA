e36a2f5d2f9e2a9ec7e5bb75def79bcf29319b3e
Hallie Dunn <hallie.dunn1@gmail.com>
2020-04-20 13:27:36 -0700

    next largest solution

--- 24-next-largest/solution.js
+++ 24-next-largest/solution.js
@@ -1,5 +1,46 @@
-//  Diana
+//  Diana, O(n^2)
 
-function nextGreatest(arr){
+function nextGreatest(array) {
+    let obj = {};
+    for (let i=0; i < array.length; i++) {
+      obj[array[i]] = array.slice(i+1).find(num => {
+        return num > array[i]
+      }) ? array.slice(i+1).find(num => {
+        return num > array[i]
+      }) : -1
+    }
+    return obj;
+}
 
+// a solution with O(n) runtime
+
+function greater(arr) {
+
+    let i = 0
+    let j = 0
+    let pointer = arr[i]
+    let current = arr[i+1]
+    while(i !== arr.length){
+  
+      if (pointer < current){
+        console.log( `${pointer} --> ${current}`)
+        i++
+        j = i+1
+        pointer = arr[i]
+        current = arr[j]
+      
+    } else if(j == arr.length){
+       console.log( `${pointer} --> -1`)
+        current = pointer
+        i++
+        j = i+1
+        pointer = arr[i]
+        current = arr[j]
+  
+      }
+      else {
+        j++
+        current = arr[j]
+      }
+    }
 }
