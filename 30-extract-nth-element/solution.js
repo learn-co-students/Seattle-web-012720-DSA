e77fb01ce77461578e1170d2ec0cb89347868fc5
Hallie Dunn <hallie.dunn1@gmail.com>
2020-04-28 15:55:01 -0700

    solutions

--- 30-extract-nth-element/solution.js
+++ 30-extract-nth-element/solution.js
@@ -1,1 +1,21 @@
 // Aidan
+function aidanNth(arr, int){
+    let arr2 = []
+
+    for(let i=0; i< arr.length; i++){
+        if(i%int != 0){
+            arr2.push(arr[i])
+        }
+    }
+
+    return arr2
+}
+
+// Jack
+
+function jackNth(array, int) {
+    for (let i = int - 1; i < array.length; i += int - 1) {
+      array.splice(i, 1);
+    }
+    return array;
+  }
