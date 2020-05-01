40ef094aafab3fc41ddaa376d2c7e3a2f6b7e3ab
Hallie Dunn <hallie.dunn1@gmail.com>
2020-05-01 10:39:23 -0700

    migratory birds

--- 31-time-conversion/solution.js
+++ 31-time-conversion/solution.js
@@ -1,1 +1,30 @@
 // Jonny
+const timeConverter = (time) => {
+    if (time.includes('P')){
+      let splitTime = time.split("P")
+      let denominations = splitTime[0].split(':')
+      denominations[0] = (parseInt(denominations[0]) + 12).toString()
+      denominations = denominations.join(':')
+      console.log(denominations)
+    }
+    if (time.includes('A')){
+      let splitAMTime = time.split("A")
+      let denominations = splitAMTime[0].split(':')
+      if (denominations[0] === '12'){
+        denominations[0] = '00'
+        denominations = denominations.join(':')
+        console.log(denominations)  
+      } else {
+      console.log(splitAMTime[0])
+      }
+    }
+  }
+
+// Clarion's same logic but plus ternary
+
+function timeConverter(time){
+    let splitTime = time.split(':')
+    time.includes("P") ? splitTime[0]=(parseInt(splitTime[0])+12).toString() : splitTime[0] == '12' ? splitTime[0] = "00" : null
+    let mil = splitTime.join(':').slice(0, 8)
+    return mil
+}
---
+++ 32-migratory-birds/README.md
@@ -0,0 +1,14 @@
+# Find the Most Repeated Number in Array
+
+- You are a field scientist counting bird sightings. Given an array of integers, each integer representing a different type of bird, determine which "bird" appears the msot times in the array and return that bird's identifying number.
+
+- If there are two birds who tie, return the bird whose index (not identifying number) is lowest.
+
+```javascript
+    imABirdScientistDoingBirdScience([1,1,2,2,3])
+        => 1
+    imABirdScientistDoingBirdScience([9,3,5,6])
+        => 9
+    imABirdScientistDoingBirdScience([3,8,1,4,8])
+        => 8
+```
