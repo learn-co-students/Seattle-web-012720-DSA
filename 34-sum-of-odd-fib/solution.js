77df92e0d9db2e7192508ac325980c52604c7a43
Hallie Dunn <hallie.dunn1@gmail.com>
2020-05-06 16:26:24 -0700

    more solutions

--- 34-sum-of-odd-fib/solution.js
+++ 34-sum-of-odd-fib/solution.js
@@ -30,3 +30,49 @@ function makeFibs (times, firstNum=1, secondNum=1, sum=0) {
     }
     return sum + 1
 }
+
+// diana
+
+function sumOddFibs(n) {
+    let x = 1;
+    let y = 1;
+    let next = 2;
+    let sum = 2;
+    while (next <= n) {
+      if (next%2 !== 0) {
+        sum += next;
+      }
+      x = y;
+      y = next;
+      next = x + y;
+    } 
+    return sum
+}
+
+// jack
+
+const fib = (digits) => {
+    let older = 1;
+    let old = 1;
+    let current;
+    let output = [older, old];
+    for(let i = 0; i < digits - 2; i++) {
+      current = older + old;
+      output.push(current);
+      older = old;
+      old = current;
+    }
+    return output;
+  }
+  const sumOddFibs= (int) => {
+    let sum = 0;
+    seq = fib(int / 2);
+    for(let i = 0; i < seq.length; i++) {
+      if(seq[i] % 2 === 1) {
+        sum += seq[i];
+      }
+      if(sum >= int) {
+        return sum;
+      }
+    }
+}
